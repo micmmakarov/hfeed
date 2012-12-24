7b5cec9c8ff2a3b12e4ad166ada4132f2f2c664f
Michael Makarov <info1117@gmail.com>
2012-12-24 14:46:12 -0800

    change fb app

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -5,8 +5,8 @@ if ENV['RAILS_ENV'] == 'development'
   FACEBOOK_CALLBACK = 'http://localhost:3000/facebook/callback'
 end
 if ENV['RAILS_ENV'] == 'production'
-  FACEBOOK_APP_ID= '306688069433056' # please change!
-  FACEBOOK_APP_SECRET= '10260ce7ccc4f6611511c20793a4673e' # please change!
-  FACEBOOK_SITE_URL = 'http://omgwant.herokuapp.com/'
-  FACEBOOK_CALLBACK = 'http://omgwant.herokuapp.com/facebook/callback'
+  FACEBOOK_APP_ID= '383953478364607 ' # please change!
+  FACEBOOK_APP_SECRET= '415e323661df8ae84ca6cc84640621a2' # please change!
+  FACEBOOK_SITE_URL = 'http://hfeed.herokuapp.com/'
+  FACEBOOK_CALLBACK = 'http://hfeed.herokuapp.com/facebook/callback'
 end
